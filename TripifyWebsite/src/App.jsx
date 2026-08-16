aa409047ea4672a74bcdc347b23577cf3e9e92b7
AkifNaveed12 <akif27031@gmail.com>
2026-08-16 21:22:04 +0500

    Tripify initial skeleton

--- TripifyWebsite/src/App.jsx
+++ TripifyWebsite/src/App.jsx
@@ -1,11 +1,30 @@
 import { useState } from 'react'
+import Header from './sections/Header'
+import Hero from './sections/Hero'
+import About from './sections/About'
+import Services from './sections/Services'
+import CtaOne from './sections/CtaOne'
+import Packages from './sections/Packages'
+import Features from './sections/Features'
+import Footer from './sections/Footer'
+
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>hello</h1>
+    <>
+    <Header />
+    <Hero />
+    <About />
+    <Services />
+    <CtaOne />
+    <Packages />
+    <Features />
+    <Footer />
+    </>
   )
 }
 
