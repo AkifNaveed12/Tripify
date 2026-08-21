c8f9c26fc841c34ab981d51a43baa6d9f1d6462d
AkifNaveed12 <akif27031@gmail.com>
2026-08-21 15:01:26 +0500

    fix: added proper content in features section

--- TripifyWebsite/src/sections/Features.jsx
+++ TripifyWebsite/src/sections/Features.jsx
@@ -17,14 +17,14 @@ const Features = () => {
             <h1 data-aos="zoom-in" data-aos-delay="100"
             className='lg:text-5xl text-3xl font-fakhwang text-black text-center'>Why should you travel with us?</h1>
             <p data-aos="zoom-in" data-aos-delay="200"
-            className='text-gray-500 text-center lg:[60%] w-full'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, alias! Perferendis cupiditate iure voluptas fuga.</p>
+            className='text-gray-500 text-center lg:w-[60%] w-full'> Thoughtful planning, trusted guidance, and dedicated support to make every journey smoother, more memorable, and truly worth taking.</p>
 
             <div data-aos="zoom-in" data-aos-delay="300"
             className=' w-full grid lg:grid-cols-3 grid-cols-1 justify-center mt-5 items-start gap-5'>
                 {features.map((item, index) => (
                     <div key={index} className='flex flex-col justify-center items-center gap-5 bg-gray-200 rounded-md lg:p-10 p-6'>
                         <span className='bg-orange-600 hover:bg-black cursor-pointer p-4 rounded-md text-white'><item.icon className='text-3xl'/></span>
-                        <h1 className='text=xl text-center font-semibold text-black'>{item.name}</h1>
+                        <h1 className='text-xl text-center font-semibold text-black'>{item.name}</h1>
 
                         <p className='text-gray-500 text-center'>{item.about}</p>
                     </div> 
